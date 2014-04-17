7ded667d6ce1c7e1e0bb6675cb0ed7d1fdb4f247
Sufyan Adam <pair+sufyan.adam@goodsearch.com>
2014-04-17 12:43:30 -0700

    No need to setup slime for now

--- init.el
+++ init.el
@@ -247,14 +247,14 @@
 (add-hook 'html-mode-hook 'skewer-html-mode)
 
 ;; Setup slime
-(load (expand-file-name "~/quicklisp/slime-helper.el"))
-(setq inferior-lisp-program "sbcl")
+;;(load (expand-file-name "~/quicklisp/slime-helper.el"))
+;;(setq inferior-lisp-program "sbcl")
 
-(require 'ac-slime)
-(add-hook 'slime-mode-hook 'set-up-slime-ac)
-(add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
-(eval-after-load "auto-complete"
-  '(add-to-list 'ac-modes 'slime-repl-mode))
+;; (require 'ac-slime)
+;; (add-hook 'slime-mode-hook 'set-up-slime-ac)
+;; (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
+;; (eval-after-load "auto-complete"
+;;   '(add-to-list 'ac-modes 'slime-repl-mode))
 
 ;; Lets start with a smattering of sanity
 (require 'sane-defaults)
